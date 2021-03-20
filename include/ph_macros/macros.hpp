93062fe11fa2da9c159de79ed7a47d7d0e142392
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2021-03-20 17:19:03 +0100

    a

--- include/ph_macros/macros.hpp
+++ include/ph_macros/macros.hpp
@@ -1,20 +1,33 @@
 #pragma once
-
+using namespace std;
 
 namespace {
-template <size_t N, size_t M> consteval bool same_strings (char const(&lhs)[N], char const(&rhs)[M]){
+template <size_t N, size_t M>
+consteval bool same_strings (char const(&lhs)[N], char const(&rhs)[M])
+{
     if (N != M)
         return false;
+    
     for (int i = 0; i < N; ++i)
     {
         if (lhs[i] != rhs[i])
             return false;
     }
+    
     return true;
 }
-#define SAME_TOKEN(x, y) __builtin_choose_expr (same_strings (BOOST_PP_STRINGIZE (x), BOOST_PP_STRINGIZE (y)), 1, 0)
-#define macro(x) __builtin_choose_expr (SAME_TOKEN (x, hej), kuk, "jo")
 
-#undef SAME_TOKEN
-#undef macro
+
+
+
+
+
+#define TOKEN_IS_STRING(x, y) __builtin_choose_expr (same_strings (BOOST_PP_STRINGIZE (x), y), 1, 0)
+#define IF_TOKEN_IS_STRING(token, string, true, false) __builtin_choose_expr (TOKEN_IS_STRING (token, string), true, false)
+}
+namespace {
+consteval auto my_function_name (char const* s = __builtin_FUNCTION ()) {
+    return s;
+}
+#define MY_FUNCTION_NAME my_function_name ()
 }
--- tests/testlib.cpp
+++ tests/testlib.cpp
@@ -3,196 +3,36 @@
 
 
 
-template <auto... values>
-struct Math
+
+TEST_CASE ("test IF_TOKEN_IS_STRING (token, string, true, false)")
 {
-    constexpr auto mean ()
-    {
-//        conjuntion_t<common_type <
-//        return ((), ...)
-    }
-};
-
-
-
-template <int i, class... T>
-concept concept_type_list_at = requires () {
-    typename type_list<T...>::iter::template at<i>::type;
-};
+#define TEST_PARAMS \
+    X (hej, "hej", 0, 1)
+    
+#define X(...) REQUIRE (IF_TOKEN_IS_STRING (__VA_ARGS__) == )
+    
+#undef TEST_PARAMS
+#undef X
+}
 
 
 
 
-namespace test_type_list
+TEST_CASE ("test SAME_TOKEN (x, y)")
 {
-TEST_CASE ( "get first element", "[type_list]" ) {
-    GIVEN ( "A type_list with some types" ) {
-        using given = type_list <string, int>;
-        REQUIRE (is_same_v <given::iter::next::iter::first::type, string>);
-    }
-}
-TEST_CASE ( "get last element", "[type_list]" ) {
-    GIVEN ( "A type_list with some types" ) {
-        using given = type_list <string, int>;
-        REQUIRE (is_same_v <given::iter::last::type, int>);
-    }
-}
-TEST_CASE ( "get next element", "[type_list]" ) {
-    GIVEN ( "A type_list with some types" ) {
-        using given = type_list <string, int, char>;
-        REQUIRE (is_same_v <given::iter::next::type, int>);
-    }
-}
-TEST_CASE ( "get element before", "[type_list]" ) {
-    GIVEN ( "A type_list with some types" ) {
-        using given = type_list <string, int, char>;
-        REQUIRE (is_same_v <given::iter::next::iter::before::type, string>);
-    }
-}
-TEST_CASE ("get size") {
-    GIVEN ("A type_list with some types") {
-        using given = type_list <string, int>;
-        REQUIRE (given::size == 2);
-    }
-}
-TEST_CASE ("get type_list at index i") {
-    GIVEN ("A type_list with some types") {
-        using TL = type_list <bool, string, int, char const*, string>;
-        SECTION ("get type_element at index") {
-            REQUIRE (is_same_v <TL::iter::at<0>::type, bool>);
-        }
-    }
-}
-TEST_CASE ("get trailing typelist") {
-    GIVEN ("A type_list with some types") {
-        using TL = type_list <int, string, int, char>;
-        REQUIRE (is_same_v<TL::iter::trailing::type, string>);
-    }
-}
-TEST_CASE ("get leading typelist") {
-    GIVEN ("A type_list with some types") {
-        using TL = type_list <int, string, int, char>;
-        REQUIRE (is_same_v<TL::iter::next::iter::leading::type, int>);
-    }
-}
-TEST_CASE ("convert typelist to tuple") {
-    GIVEN ("A type_list with some types") {
-        using TL = type_list <int, string, int, char>;
-        REQUIRE (is_same_v<TL::iter::next::iter::leading::type, int>);
-    }
-    GIVEN ("A non-starting typelist index") {
-        using TL = type_list <int, string, int, char>;
-        REQUIRE (is_same_v <TL::iter::next::tuple, tuple <string, int, char>>);
-    }
-}
-TEST_CASE ("use tuple as template parameters for type_list") {
-    GIVEN ("A type_list with some types") {
-        using TL = type_list <int, string, int, char>;
-        REQUIRE (is_base_of_v <TL, type_list <tuple <int, string, int, char>>>);
-        REQUIRE (is_same_v <type_list <tuple <int, string, int, char>>::iter::next::type, string>);
-    }
-}
-TEST_CASE ("use typelist as template parameter for std::tuple") {
-    GIVEN ("A type_list with some types") {
-        using TL = type_list <int, string, int, char>;
-        REQUIRE (is_base_of_v <tuple <int, string, int, char>, tuple <TL>>);
-    }
-}
-TEST_CASE ("split typelist into 2 typelists") {
-    GIVEN ("A type_list with some types") {
-        
-        #define TMP_PARAMS_LHS int, string, char
-        #define TMP_PARAMS_RHS int, char
-
-        REQUIRE (is_same_v <invoke_result_t <decltype (split_type_list <3, TMP_PARAMS_LHS, TMP_PARAMS_RHS>), type_list <TMP_PARAMS_LHS, TMP_PARAMS_RHS>>, type_list <TMP_PARAMS_LHS, TMP_PARAMS_RHS>>);
-            
-        #undef TMP_PARAMS_CALL
-        #undef TMP_PARAMS_RESULT
-    }
-}
-TEST_CASE ("pop typelist element") {
-    GIVEN ("A type_list with some types") {
-        using pop_type = char;
-        
-        REQUIRE (is_same_v <invoke_result_t <decltype (pop_type_list <0, pop_type, int, char>), type_list <pop_type, int, char>>, type_list <int, char>>);
-        REQUIRE (is_same_v <invoke_result_t <decltype (pop_type_list <1, int, pop_type, char>), type_list <int, pop_type, char>>, type_list <int, char>>);
-        REQUIRE (is_same_v <invoke_result_t <decltype (pop_type_list <2, int, char, pop_type>), type_list <int, char, pop_type>>, type_list <int, char>>);
-        REQUIRE (is_same_v <invoke_result_t <decltype (pop_type_list <0, pop_type>), type_list <pop_type>>, type_list <>>);
-    }
-}
-//TEMPLATE_PRODUCT_TEST_CASE_METHOD_SIG(Template_Fixture_2, "A", "[", ((typename T, size_t S), T, S),(std::array, Template_Foo_2), ((int,2), (float,6)))
-
-TEST_CASE ("Testing type_list::iter::at")
-{
-    #define TYPE_LIST_PARAMS \
-        int, \
-        char, \
-        string, \
-        char const*
+    #define TEST_TOKEN \
+        X (x) \
+        X (abs) \
+        X (hej då)
     
-    #define TYPE_LIST \
-        type_list <TYPE_LIST_PARAMS>
+    #define X(token) \
+        REQUIRE (TOKEN_IS_STRING (token, BOOST_PP_STRINGIZE (token)));
     
-    SECTION ("Should not work")
-    {
-        GIVEN (BOOST_PP_STRINGIZE (TYPE_LIST))
-        {
-            SECTION ("call with -1")
-            {
-                REQUIRE (concept_type_list_at <-1, TYPE_LIST_PARAMS> == 0);
-            }
-            SECTION ("call with 3")
-            {
-                REQUIRE (concept_type_list_at <TYPE_LIST::size, TYPE_LIST_PARAMS> == 0);
-            }
-        }
-    }
+    TEST_TOKEN
     
-    SECTION ("Should work")
-    {
-        GIVEN (BOOST_PP_STRINGIZE (TYPE_LIST))
-        {
-            SECTION ("call with 0")
-            {
-                REQUIRE (concept_type_list_at <0, TYPE_LIST_PARAMS> == 1);
-            }
-            SECTION ("call with elem = size-1")
-            {
-                REQUIRE (concept_type_list_at <TYPE_LIST::size - 1, TYPE_LIST_PARAMS> == 1);
-            }
-        }
-    }
-    
-    #undef TYPE_LIST
-    #undef TYPE_LIST_PARAMS
-    
-    
-    
+    #undef TEST_TOKEN
+    #undef X
 }
-
-}
-
-
-//int main ()
-//{
-//
-//    static_assert(is_same_v <type_list <string, int>::last, int>, "fel");
-//
-//
-//
-//
-//
-//    return 0;
-//}
-
-
-
-
-
-
-
-
-
 
 
 
@@ -201,11 +41,7 @@ TEST_CASE ("Testing type_list::iter::at")
 
 
 int main( int argc, char* argv[] ) {
-  // global setup...
-//    cout << type_list <int, char, bool> {} << endl;
-//    cout << pop_type_list <2> (type_list <int, char, bool> {}) << endl;
 
-//    cout << concept_type_list_at<-1, int, char> << endl;
     int result = Catch::Session().run( argc, argv );
 
   // global clean-up...
