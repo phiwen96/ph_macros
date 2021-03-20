3c4ebf663d53cc89534b20052a7eec81eb2a0ca4
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2021-03-20 19:55:16 +0100

    a

--- include/ph_macros/macros.hpp
+++ include/ph_macros/macros.hpp
@@ -17,13 +17,76 @@ consteval bool same_strings (char const(&lhs)[N], char const(&rhs)[M])
     return true;
 }
 
+template <size_t N>
+consteval size_t string_size (char const(&)[N])
+{
+    return N;
+}
 
 
 
+#define PH_$empty()
 
 
-#define TOKEN_IS_STRING(x, y) __builtin_choose_expr (same_strings (BOOST_PP_STRINGIZE (x), y), 1, 0)
-#define IF_TOKEN_IS_STRING(token, string, true, false) __builtin_choose_expr (TOKEN_IS_STRING (token, string), true, false)
+
+/*
+ delay the evaluation of a macro
+ 
+ @example
+    foo (x, y) x,  y
+    bar () 1, 2
+        
+        foo (bar ())            ERROR
+        defer (foo) (bar ())    foo (1, 2)
+ DEFER(A)() expands to A()
+ */
+#define PH_$defer(...) __VA_ARGS__ PH_$empty ()
+
+#define EAT(...)
+#define EXPAND(...) __VA_ARGS__
+#define EMPTY()
+
+
+
+#define IF_0_ELSE(...) EXPAND
+#define IF_1_ELSE(...) __VA_ARGS__ EAT
+#define IF_ELSE(cond) IF_ ## cond ##_ELSE
+
+#define IF_0(...)
+#define IF_1(...) __VA_ARGS__
+#define IF(cond) IF_ ## cond
+
+
+
+#define PH_MACRO_$expand(...) __VA_ARGS__
+
+#define PH_MACRO_$string_size(x) string_size (x)
+
+#define PH_MACRO(...) __VA_ARGS__
+
+
+#define TO_UPPERCASE_COUNTED(Str,Cnt)
+#define TO_UPPER(Str) TO_UPPERCASE_COUNTED(Str,__COUNT__) {( \
+   static char buf_##Cnt[sizeof(Str)+4]; \
+   char *str_##Cnt = Str; \
+   int ix_##Cnt = 0; \
+   for (; *str_##Cnt; str_##Cnt++, ix_##Cnt++) \
+     if (ix_##Cnt < sizeof(buf_##Cnt)-1) \
+         buf_##Cnt[ix_##Cnt] = toupper(*str_##Cnt); \
+   buf_##Cnt; )}
+
+
+
+//#define TO_UPPER(x)
+
+
+#define PH_MACRO_$token_is_string(x, y) __builtin_choose_expr (same_strings (BOOST_PP_STRINGIZE (x), y), 1, 0)
+#define PH_MACRO_$if_token_is_string(token, string, true, false) __builtin_choose_expr (PH_MACRO_$token_is_string (token, string), true, false)
+#define PH_MACRO_$has(x) \
+    [] (auto a) constexpr { \
+        return requires (){a.x;}; \
+    }
+
 }
 namespace {
 consteval auto my_function_name (char const* s = __builtin_FUNCTION ()) {
--- tests/testlib.cpp
+++ tests/testlib.cpp
@@ -1,15 +1,22 @@
 #include "headers.hpp"
 
 
+#
 
 
 
 TEST_CASE ("test IF_TOKEN_IS_STRING (token, string, true, false)")
 {
-#define TEST_PARAMS \
-    X (hej, "hej", 0, 1)
+    REQUIRE (PH_MACRO_$if_token_is_string (hej, "hej", 24, 0) == 24);
+    REQUIRE (PH_MACRO_$if_token_is_string (kuk, "hej", 0, 24) == 24);
     
-#define X(...) REQUIRE (IF_TOKEN_IS_STRING (__VA_ARGS__) == )
+#define TEST_PARAMS \
+    X (hej, "hej", 0, 0)
+    
+#define X(...) REQUIRE (IF_TOKEN_IS_STRING (__VA_ARGS__) == BOOST_PP_VARIADIC_ELEM (BOOST_PP_VARIADIC_SIZE (__VA_ARGS__) - 1, __VA_ARGS__));
+    
+
+//    TEST_PARAMS
     
 #undef TEST_PARAMS
 #undef X
@@ -18,33 +25,82 @@ TEST_CASE ("test IF_TOKEN_IS_STRING (token, string, true, false)")
 
 
 
-TEST_CASE ("test SAME_TOKEN (x, y)")
+TEST_CASE ("test TOKEN_IS_STRING (x, y)")
 {
-    #define TEST_TOKEN \
-        X (x) \
-        X (abs) \
-        X (hej då)
+    REQUIRE (PH_MACRO_$token_is_string (kuk, "kuk") == 1);
+    REQUIRE (PH_MACRO_$token_is_string (hej, "phi") == 0);
     
-    #define X(token) \
-        REQUIRE (TOKEN_IS_STRING (token, BOOST_PP_STRINGIZE (token)));
-    
-    TEST_TOKEN
-    
-    #undef TEST_TOKEN
-    #undef X
+//    #define TEST_TOKEN \
+//        X (x) \
+//        X (abs) \
+//        X (hej då)
+//
+//    #define X(token) \
+//        REQUIRE (TOKEN_IS_STRING (token, BOOST_PP_STRINGIZE (token)));
+//
+//    TEST_TOKEN
+//
+//    #undef TEST_TOKEN
+//    #undef X
 }
 
 
 
+namespace ph {
+namespace macro {
+struct token_is_string
+{
+#define PH_MACRO_TOKEN_IS_STRING
+};
+}
+}
 
 
+#define TEXT1(z, n, text) BOOST_PP_COMMA_IF(n) text
 
+#define TEXT2(z, n, text) \
+BOOST_PP_COMMA_IF (n) \
+      BOOST_PP_REPEAT_ ## z( \
+         BOOST_PP_INC(n), \
+         TEXT, text [n] \
+      )
+   /**/
+
+#define TEXT3(x, n) BOOST_PP_CAT (BOOST_PP_REPEAT_, n)(n, BOOST_PP_STRINGIZE (x),  PH_MACRO_$expand(nil))
+//#define TEXT4(x) TEXT3 (x, PH_MACRO_$string_size (x))
+
+#define A0(x) x + 2
+#define A1(x) x + 1
+
+template <char...>
+struct Test{};
 
 int main( int argc, char* argv[] ) {
 
-    cout << BOOST_TTI_NAMESPACE
+#define MACRO(x) IF_TOKEN_IS_STRING (x, "ph::", cout << "ph", cout << "not ph")
+    
+//    Test<TEXT3("hej", 3)> t1;
+//    cout << A1(A2(1)) << endl;
+   
+    PH_$empty()
+//    IF(1)(cout << "1", cout << "0");
+    
+    IF(1)(cout << "hi" << endl;)
+    
+    IF_ELSE (1)
+        (cout << "1")
+        (cout << "0");
+    
+    
+    
+    cout << endl;
+//    cout << BOOST_TTI_NAMESPACE
     int result = Catch::Session().run( argc, argv );
-
+    string a;
+    using ty = int;
+//    template <class T>
+    bool has_toString = PH_MACRO_$has (c_str()) (a);
+    cout << has_toString << endl;
   // global clean-up...
 
     return result;
